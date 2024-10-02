8ca1792d293dfb75d1ec5f524726305d521f3f80
WilmerProfesor <wilmerprofesor@gmail.com>
2024-10-02 09:10:15 -0500

    sesion DOM

--- Tema10-DOM/js/main.js
+++ Tema10-DOM/js/main.js
@@ -3,13 +3,6 @@ let b=document.getElementById("b");
 let buttonAdd=document.getElementById("buttonAdd");
 let c=document.getElementById("c");
 
-// function suma(){
-//     c.value=Number(a.value)+Number(b.value)
-// }
-
-// buttonAdd.addEventListener("click",suma());
-
-
 buttonAdd.addEventListener("click",(ev)=>{
     alert(a.value+b.value);
     c.value=Number(a.value)+Number(b.value)
